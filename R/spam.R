22213d6c90bc179fda61cd0b0b30a889e8460064
nicholasjhorton <nicholasjhorton@gmail.com>
2020-08-20 09:22:02 -0400

    Added some documentation for the spam datasets.

--- R/spam.R
+++ R/spam.R
@@ -1,16 +1,40 @@
-#' Headlines
+#' Email Subject Lines
 #'
-#' This data comes from Chakraborty et. al., which combines headlines from
-#' a variety of news and clickbait sources. Much of these headlines contain
-#' subject matter innapropriate for classroom use. Given the volume of headlines
-#' containing such language (especially for clickbait == TRUE), this filtering
-#' might not catch all problematic headlines. User discretion is advised.
+#' This dataset includes a set of email subject lines used for classification
+#' of whether the message is spam (unsolicited commercial content) or not.
+#' Many subject lines include  subject matter innapropriate for classroom use. 
+#' Given the volume of headlines containing such language 
+#' (especially for spam == TRUE), user discretion is advised.
 #'
-#' @format A data frame with 22949 rows and 3 variables:
+#' @format A data frame with 6,908 rows and 3 variables:
 #' \describe{
-#'   \item{title}{String}
-#'   \item{clickbait}{Boolean}
+#'   \item{subjectline}{String}
+#'   \item{type}{Boolean}
 #'   \item{ids}{Integer}
 #' }
-#' @source \url{https://github.com/bhargaviparanjape/clickbait}
-"headlines"
+#' @source \url{http://www.rdatasciencecases.org/Spam}
+"emails"
+#' Email Train
+#'
+#' This dataset is a random sample of 80% of the emails data.
+#'
+#' @format A data frame with 5,526 rows and 3 variables:
+#' \describe{
+#'   \item{subjectline}{String}
+#'   \item{type}{Boolean}
+#'   \item{ids}{Integer}
+#' }
+#' @source \url{http://www.rdatasciencecases.org/Spam}
+"emails_train"
+#' Email Test
+#'
+#' This dataset is a random sample of 20% of the emails data.
+#'
+#' @format A data frame with 1,382 rows and 3 variables:
+#' \describe{
+#'   \item{subjectline}{String}
+#'   \item{type}{Boolean}
+#'   \item{ids}{Integer}
+#' }
+#' @source \url{http://www.rdatasciencecases.org/Spam}
+"emails_test"
